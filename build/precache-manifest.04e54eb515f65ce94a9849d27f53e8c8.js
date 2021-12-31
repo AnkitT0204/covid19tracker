fe78fc8f1c1544155d82b2797587d39ec6fbea4d
Ankit Tanwar <ankitt0204@gmail.com>
2021-12-31 21:11:02 +0530

    initial commit

---
+++ build/precache-manifest.04e54eb515f65ce94a9849d27f53e8c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b22143aaeea756a941f1145f96f271",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b6d4c268a77e9e52c549",
+    "url": "/static/css/main.411e4345.chunk.css"
+  },
+  {
+    "revision": "ed5874af5ddd9d27c8fd",
+    "url": "/static/js/2.ad8e3933.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.ad8e3933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d4c268a77e9e52c549",
+    "url": "/static/js/main.6fffe15f.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
